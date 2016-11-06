a068601a8159fe894bc738d01c146b10a68cd542
Vadym Kalin <kalinvadim@gmail.com>
2016-11-06 22:13:18 +0200

    Fixed translations

--- app/code/community/Kalin/GoogleKnowledgeGraph/Model/Adminhtml/System/Config/Source/Contacttype.php
+++ app/code/community/Kalin/GoogleKnowledgeGraph/Model/Adminhtml/System/Config/Source/Contacttype.php
@@ -30,17 +30,17 @@ class Kalin_GoogleKnowledgeGraph_Model_Adminhtml_System_Config_Source_Contacttyp
     public function toOptionArray()
     {
         return array(
-            array('value' => 'customer-support',    'label' => Mage::helper('googleknowledgegraph')->__('customer support')),
-            array('value' => 'technical-support',   'label' => Mage::helper('googleknowledgegraph')->__('technical support')),
-            array('value' => 'billing-support',     'label' => Mage::helper('googleknowledgegraph')->__('billing support')),
-            array('value' => 'bill-payment',        'label' => Mage::helper('googleknowledgegraph')->__('bill payment')),
-            array('value' => 'sales',               'label' => Mage::helper('googleknowledgegraph')->__('sales')),
-            array('value' => 'reservations',        'label' => Mage::helper('googleknowledgegraph')->__('reservations')),
-            array('value' => 'credit-card-support', 'label' => Mage::helper('googleknowledgegraph')->__('credit card support')),
-            array('value' => 'emergency',           'label' => Mage::helper('googleknowledgegraph')->__('emergency')),
-            array('value' => 'baggage-tracking',    'label' => Mage::helper('googleknowledgegraph')->__('baggage tracking')),
-            array('value' => 'roadside-assistance', 'label' => Mage::helper('googleknowledgegraph')->__('roadside assistance')),
-            array('value' => 'package-tracking',    'label' => Mage::helper('googleknowledgegraph')->__('package tracking')),
+            array('value' => 'customer-support',    'label' => Mage::helper('googleknowledgegraph')->__('Customer support')),
+            array('value' => 'technical-support',   'label' => Mage::helper('googleknowledgegraph')->__('Technical support')),
+            array('value' => 'billing-support',     'label' => Mage::helper('googleknowledgegraph')->__('Billing support')),
+            array('value' => 'bill-payment',        'label' => Mage::helper('googleknowledgegraph')->__('Bill payment')),
+            array('value' => 'sales',               'label' => Mage::helper('googleknowledgegraph')->__('Sales')),
+            array('value' => 'reservations',        'label' => Mage::helper('googleknowledgegraph')->__('Reservations')),
+            array('value' => 'credit-card-support', 'label' => Mage::helper('googleknowledgegraph')->__('Credit card support')),
+            array('value' => 'emergency',           'label' => Mage::helper('googleknowledgegraph')->__('Emergency')),
+            array('value' => 'baggage-tracking',    'label' => Mage::helper('googleknowledgegraph')->__('Baggage tracking')),
+            array('value' => 'roadside-assistance', 'label' => Mage::helper('googleknowledgegraph')->__('Roadside assistance')),
+            array('value' => 'package-tracking',    'label' => Mage::helper('googleknowledgegraph')->__('Package tracking')),
         );
     }
 }
