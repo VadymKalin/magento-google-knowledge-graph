442f9014efd85dd3333d0ab578cd01287fab3d35
Vadym Kalin <kalinvadim@gmail.com>
2017-01-11 08:56:25 +0100

    to bring code to standard PSR-4

--- app/code/community/Kalin/GoogleKnowledgeGraph/Helper/Data.php
+++ app/code/community/Kalin/GoogleKnowledgeGraph/Helper/Data.php
@@ -119,5 +119,4 @@ class Kalin_GoogleKnowledgeGraph_Helper_Data extends Mage_Core_Helper_Abstract
     {
         return Mage::getStoreConfig(self::XML_PATH_GKG_AVAILABLELANGUAGE, $storeId);
     }
-
 }
